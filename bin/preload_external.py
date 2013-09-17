650336e3c933fab92e4add9c65eacfacaecb53a5
tmetsch <tmetsch@gmai.com>
2013-09-17 10:20:22 +0200

    matplotlib configs.

--- bin/preload_external.py
+++ bin/preload_external.py
@@ -8,8 +8,12 @@ __author__ = 'tmetsch'
 
 from matplotlib import pyplot
 
-params = {'legend.fontsize': 11,
-          'legend.linewidth': 1}
+params = {'legend.fontsize': 9.0,
+          'legend.linewidth': 0.5,
+          'font.size': 9.0,
+          'axes.linewidth': 0.5,
+          'lines.linewidth': 0.5,
+          'grid.linewidth':   0.5}
 
 fig = pyplot.figure(1, figsize=(3, 3))
 pyplot.rcParams.update(params)
