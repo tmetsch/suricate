964f8e6186819baed9081f170aa4d3d25d78a188
Thijs Metsch <tmetsch@engjoy.eu>
2015-01-17 19:00:52 +0100

    spacebar

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(name='suricate',
       author='Thijs Metsch',
       author_email='tmetsch@engjoy.eu',
       url='http://www.engjoy.eu',
-      packages=['suricate','suricate.ui', 
+      packages=['suricate', 'suricate.ui',
                 'suricate.data', 'suricate.analytics'],
       # for prod also requires whatever you put in sdk.
       requires=['bottle', 'pymongo', 'matplotlib', 'pika', 'mox'])
