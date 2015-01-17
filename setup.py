d03b81e146be239184dabe604d1145929f9d5d71
Thijs Metsch <tmetsch@engjoy.eu>
2015-01-17 18:16:31 +0100

    minor tweak :-)

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ __author__ = 'tmetsch'
 from distutils.core import setup
 
 setup(name='suricate',
-      version='1.0',
-      description='Analytics Service',
+      version='1.1',
+      description='Open Source Cloud-Native Data Science Platform',
       author='Thijs Metsch',
       author_email='tmetsch@engjoy.eu',
       url='http://www.engjoy.eu',
