295fced4f8a5a247e63d4e4cf5a7fcd19f923ab0
Thijs Metsch <tmetsch@engjoy.eu>
2015-01-17 18:29:25 +0100

    minor packaging fix

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(name='suricate',
       author='Thijs Metsch',
       author_email='tmetsch@engjoy.eu',
       url='http://www.engjoy.eu',
-      packages=['suricate.ui', 'suricate.data', 'suricate.analytics'],
+      packages=['suricate','suricate.ui', 
+                'suricate.data', 'suricate.analytics'],
       # for prod also requires whatever you put in sdk.
       requires=['bottle', 'pymongo', 'matplotlib', 'pika', 'mox'])
